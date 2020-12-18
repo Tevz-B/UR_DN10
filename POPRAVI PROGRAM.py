a99a35b51048344bb50085189a69006937118736
Tevz Basaj <tb7543@student.uni-lj.si>
2020-12-18 10:01:50 +0100

    podveja, Deljenje popravljeno

--- POPRAVI PROGRAM.py
+++ POPRAVI PROGRAM.py
@@ -1,11 +1,11 @@
 # Deljenje
 
-# POTREBNO POPRAVITI
+
 
 def deli(a, b, mode): # deli a z b, mode je lahko 0 ali 1,
     # 0 je navadno deljenje, 1 je celoštevilsko, drugače vrne None
 
-    # Ne dela če deliš z 0, kar je treba popraviti
+
     try:
         if mode == 0:
             return a/b
@@ -13,7 +13,7 @@ def deli(a, b, mode): # deli a z b, mode je lahko 0 ali 1,
             return a//b
         else:
             return None
-    except TypeError:
+    except:
         return None
 
 print(deli(42, 0, 1))
